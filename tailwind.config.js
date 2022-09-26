612eb2433e319f300d4947c5cd511914a2c0bc52
t <husam@Husams-MacBook-Pro.local>
2022-09-26 10:21:49 +0300

    Add existing project files to Git

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors:{
+      'qred':'#8D1A3D',
+      'white':'#fff',
+      'black':'#000',
+      'qblue':'#579BD3',
+      'sblue':'#F0F8FF',
+      'dblue':'#0A142F',
+      'qyellow':'#FFE2A7',
+    },
+    fontFamily:{
+      'body':['Qatar'],
+      
+    },
+    extend: {},
+  },
+  plugins: [],
+}
